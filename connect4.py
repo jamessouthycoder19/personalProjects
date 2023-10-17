4eb70aed725f1baf089e0842f5f5ac5ed439614a
James Southcott <James.southcott19@gmail.com>
2023-10-16 21:37:26 -0400

    print board done

---
+++ connect4.py
@@ -0,0 +1,19 @@
+def print_board(board):
+    for i in range(6):
+        for j in range(7):
+            print(board[i][j],end=' ')
+        print()
+
+def check_win():
+    ...
+
+def turn():
+    ...
+
+def main():
+    board = [['-'for i in range(7)]for j in range(6)]
+    player_one_wins = 0
+    player_two_wins = 0
+
+if __name__ == "__main__":
+    main()
