a508f0441a7579506434275c11fe25bb83dba418
James Southcott <James.southcott19@gmail.com>
2023-11-18 14:43:41 -0500

    initial commit

---
+++ wordle.py
@@ -0,0 +1,11 @@
+import random
+
+def check_guess(guess):
+    ...
+
+def main():
+    words = ["happy","click","fleet","dirty","alive","flick","think","mouse","sling","thing","lorax"]
+    user_word = words[random.randrange(len(words))]
+
+if __name__ == "__main__":
+    main()
